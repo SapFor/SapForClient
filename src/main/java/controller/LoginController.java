d980158351ceabef7c673ebfc0e777976a8e6f3e
CChabert <chabert.caroline@gmail.com>
2015-03-09 14:03:12 +0100

    LoginController First step

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LoginController {
+
+    @FXML
+    private Button validerLoginButton;
+
+    @FXML
+    void onClicValiderLogin(Event event) {
+
+    }
+
+}
