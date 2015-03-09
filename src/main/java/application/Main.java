a4d2bdaab3f8e0132d85d280dae6f08fffb4d29a
CChabert <chabert.caroline@gmail.com>
2015-03-09 17:52:44 +0100

    Updates about ClientApp

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
+			//Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
