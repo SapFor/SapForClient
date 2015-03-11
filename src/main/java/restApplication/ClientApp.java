bbcc450633ae1e3c51cd1296a247be734763becf
lmalg <leomalgrange@gmail.com>
2015-03-11 16:03:19 +0100

    Revert "Test pour listes vide ClientApp"
    This reverts commit dfc8f091e2122bc7c060f882042d710122c2952d.

--- src/main/java/restApplication/ClientApp.java
+++ src/main/java/restApplication/ClientApp.java
@@ -137,20 +137,18 @@ public class ClientApp {
 			String nomLieu;
 			String ligneSess;
 	    	
-			if(listSessionDir != null){
-				Iterator<StageConcret> ite = listSessionDir.iterator();
-		    	while(ite.hasNext()){  // loop to get name/date/place of each stage and put into the created list
-		    		StageConcret newLigne = ite.next();
-					
-					nomUV = newLigne.getUV();
-					dateStage = newLigne.getDate();
-					date = dateStage.get(Calendar.DAY_OF_MONTH) + "/" + (dateStage.get(Calendar.MONTH)+1) + "/" + dateStage.get(Calendar.YEAR);
-					nomLieu = newLigne.getLieu();
-		    		ligneSess = nomUV + "\t" + date + "\t" + nomLieu;
-		    		listSess.add(ligneSess);
-		    		tableDeCorrespondance.put(ligneSess, newLigne);
-		    	}
-			}
+			Iterator<StageConcret> ite = listSessionDir.iterator();
+	    	while(ite.hasNext()){  // loop to get name/date/place of each stage and put into the created list
+	    		StageConcret newLigne = ite.next();
+				
+				nomUV = newLigne.getUV();
+				dateStage = newLigne.getDate();
+				date = dateStage.get(Calendar.DAY_OF_MONTH) + "/" + (dateStage.get(Calendar.MONTH)+1) + "/" + dateStage.get(Calendar.YEAR);
+				nomLieu = newLigne.getLieu();
+	    		ligneSess = nomUV + "\t" + date + "\t" + nomLieu;
+	    		listSess.add(ligneSess);
+	    		tableDeCorrespondance.put(ligneSess, newLigne);
+	    	}
 	    	return listSess;
 	    }
 		
@@ -294,17 +292,18 @@ public class ClientApp {
 			
 			List<String> listUVDispo = new ArrayList<String>(); // create list of UV for pushing on the tab
 			String nomUV;
-			if(listUV != null){		
-				Iterator<UVConcret> ite = listUV.iterator();
-				while(ite.hasNext()){  // loop to get name of each UV and put into the created list
-					UVConcret newLigne = ite.next();
-					nomUV = newLigne.getNom();
-					listUVDispo.add(nomUV);
-							
-				}
+					
+			Iterator<UVConcret> ite = listUV.iterator();
+			while(ite.hasNext()){  // loop to get name of each UV and put into the created list
+				UVConcret newLigne = ite.next();
+				nomUV = newLigne.getNom();
+				listUVDispo.add(nomUV);
+						
 			}
 			return listUVDispo;
 		}
+		
+		
 		
 		// Get list of the formation UVs, formateur radioButton : to put into the formation tab
 		public static List<String> getListUVFormateur(){
@@ -314,14 +313,13 @@ public class ClientApp {
 						
 			List<String> listUVDispo = new ArrayList<String>(); // create list of UV for pushing on the tab
 			String nomUV;
-			if(listUV != null){
-				Iterator<UVConcret> ite = listUV.iterator();
-				while(ite.hasNext()){  // loop to get name of each UV and put into the created list
-					UVConcret newLigne = ite.next();
-					nomUV = newLigne.getNom();
-					listUVDispo.add(nomUV);
-										
-				}
+								
+			Iterator<UVConcret> ite = listUV.iterator();
+			while(ite.hasNext()){  // loop to get name of each UV and put into the created list
+				UVConcret newLigne = ite.next();
+				nomUV = newLigne.getNom();
+				listUVDispo.add(nomUV);
+									
 			}
 			return listUVDispo;
 		}
